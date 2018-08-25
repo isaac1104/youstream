bb71d2784899f770ed102478c728489eabe02828
isaac1104 <isaac-kwon@hotmail.com>
2018-08-25 15:34:27 -0700

    greet message change

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -17,11 +17,8 @@ const About = () => {
   return (
     <div style={style}>
       <div>
-        <h1>Hi! My name is Isaac.</h1>
-        <h2>Youstreamer is a web application that looks a lot like Youtube. Well... this is not a coincidence.</h2>
-        <h2>The purpose of this project was to sharpen my React and Redux skills, while creating a fun project because I love coding! </h2>
-        <h2>Please feel free to search for awesome videos and also take a look at my portfolio by clicking the link on the sidebar. Thank you!</h2>
-        <p>- Isaac Kwon</p>
+        <h1>Welcome to Youstreamer, a mini Youtube Clone</h1>
+        <h2>Search for your favorite video!</h2>
       </div>
     </div>
   );
