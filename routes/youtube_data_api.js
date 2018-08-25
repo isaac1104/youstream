56952efec67930aadbbaeb5b4a56a91f13bf1ea1
isaac1104 <isaac-kwon@hotmail.com>
2018-08-25 16:48:37 -0700

    fetch api data from express

--- routes/youtube_data_api.js
+++ routes/youtube_data_api.js
@@ -2,13 +2,9 @@ const axios = require('axios');
 const keys = require('../config/keys');
 
 module.exports = app => {
-  app.get('/api/video', (req, res) => {
-    res.send('hi');
-    // const request = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&videoSyndicated=true&q=${req.query.keyword}&key=${req.query.key}`);
-    // console.log(request);
-  });
-
-  app.get('/api/test', (req, res) => {
-    res.send('test');
+  app.get('/api/data', async (req, res) => {
+    const request = await axios.get(`${keys.youtubeApiURL}${req.query.keyword}&key=${keys.youtubeApiKey}`);
+    const { data } = request;
+    res.send(data);
   });
 };
