776b63a54f5f6d24afee13bde82a044b1f3c5fac
isaac1104 <isaac-kwon@hotmail.com>
2018-08-25 17:28:36 -0700

    scroll to top add

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,21 +2,22 @@ import React from "react";
 import Navbar from "./../containers/Navbar";
 import VideoDetails from "./VideoDetails";
 import About from "./../pages/About";
+import ScrollToTop from './ScrollToTop';
 import VideosContainer from "./../containers/VideosContainer";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
-      <div>
+    <BrowserRouter>
+      <ScrollToTop>
         <Navbar/>
         <Switch>
           <Route exact path="/" component={About} />
           <Route exact path="/videos/search/:keyword" component={VideosContainer} />
           <Route exact path="/videos/:id" component={VideoDetails} />
-      </Switch>
-    </div>
-    </Router>
+        </Switch>
+      </ScrollToTop>
+    </BrowserRouter>
   );
 }
 
